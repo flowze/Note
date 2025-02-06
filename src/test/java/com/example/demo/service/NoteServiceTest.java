e10ecf19b72abdf13139bc8f78bc528aab2d4f2e
Nikolay <frizkw@gmail.com>
2025-02-06 13:37:38 +0700

    addtests

---
+++ src/test/java/com/example/demo/service/NoteServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoteServiceTest {
+
+    @Test
+    void findByUser() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void updateNote() {
+    }
+
+    @Test
+    void deleteNote() {
+    }
+
+    @Test
+    void createNote() {
+    }
+
+    @Test
+    void deleteImage() {
+    }
+
+    @Test
+    void updateOrder() {
+    }
+}
